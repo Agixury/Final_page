01dcaabb754a2c919d015264a7015dd28ec5fbc1
Srikar <48818698+srikar-kotra@users.noreply.github.com>
2024-03-01 01:23:03 -0600

    Update renders.js

--- routes/renders.js
+++ routes/renders.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
 	return res.render('index');
 });
-
+// Added code for stability
 router.get('/hotels', (req, res) => {
 	return res.render('hotels');
 });
